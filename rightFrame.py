e099b1f79c42fc1b25c44e81ea59bdc8897eb674
Andrew Louis Hermo <alrhermo@gmail.com>
2022-09-28 21:43:07 +0800

    Fix frame size

--- rightFrame.py
+++ rightFrame.py
@@ -1,17 +1,18 @@
 from tkinter import LabelFrame, Listbox
 
-# Constants
-WINDOW_DIMENSIONS = (400, 950)
-
 
 # Frame that displays the metadata/headers
 class RightFrame:
     def __init__(self, parent):
         # Root Window
         self.parent = parent
+        self.parent.update()
+
+        # Constants
+        window_dimensions = (400, self.parent.winfo_height())
 
         # Instantiate frame
-        self.right_frame = LabelFrame(self.parent, width=WINDOW_DIMENSIONS[0], height=WINDOW_DIMENSIONS[1], bg="grey",
+        self.right_frame = LabelFrame(self.parent, width=window_dimensions[0], height=window_dimensions[1], bg="grey",
                                       borderwidth=2, relief="raised",
                                       text="Image Header Metadata")
         self.right_frame.grid(column=1, row=0, pady=10, padx=10, ipadx=5, ipady=5)
@@ -20,7 +21,7 @@ class RightFrame:
         self.show_right_frame = True
 
         # Contains the list of header metadata
-        self.header_list = Listbox(self.right_frame, height=52, width=45)
+        self.header_list = Listbox(self.right_frame, height=45, width=40)
         self.header_list.grid(column=0, row=0)
 
     # Add the headers to be displayed in the screen
