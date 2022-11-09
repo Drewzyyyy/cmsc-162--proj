be7a635a0f32794aebda4a2aa8d63bc8f6e8b707
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2022-11-09 14:19:25 +0800

    guide5 comments done - spatial domain

--- utils.py
+++ utils.py
@@ -105,6 +105,7 @@ def generate_bw(threshold: int = 127):
 
 
 # Generate an image with law gamma filter based on the image uploaded
+# Gets the user input regarding the gamma constant/value as a parameter
 def generate_law_gamma(gamma_const: float = 0.4):
     img = cv2.imread('./assets/pic.png')
 
@@ -113,37 +114,45 @@ def generate_law_gamma(gamma_const: float = 0.4):
                      ** gamma_const, dtype='uint8')
     return get_imagetk(gamma)
 
-
+# Generates image w/ salt and pepper noise
+# Gets the image as a parameter
 def salt_and_pepper(img):
     rows, cols = img.shape
-
+    
+    # Randomly generates random number of pixels to be affected by the salt and pepper noise
     num_pix = randint(300, 10000)
 
     # Change to white
     for i in range(num_pix):
-        y = randint(0, rows - 1)
+        # choose random coordinates
+        y = randint(0, rows - 1) 
         x = randint(0, cols - 1)
-        img[y][x] = 255
+        img[y][x] = 255 # turn random pixel to white
 
     # Change to black
-    num_pix = randint(300, 10000)
+    num_pix = randint(300, 10000) # Randomly generates random number of pixels to be affected by the salt and pepper noise
     for i in range(num_pix):
+        # choose random coordinates
         y = randint(0, rows - 1)
         x = randint(0, cols - 1)
-        img[y][x] = 0
+        img[y][x] = 0 # turn random pixel to black
 
     cv2.imwrite('./assets/salt_and_pepper.png', img)
     return get_imagetk(img)
 
-
+# Generates image w/ averaging filter
+# Gets the image as a parameter
 def generate_averaging_filter(img):
-    rows, cols = img.shape  # rows & columns
+    rows, cols = img.shape  # gets nos. of rows & columns
 
+    # Weighted average mask
     mask = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
-    mask = mask / 16
+    mask = mask / 16 # normalization
 
+    # Creates new array for new image w/ filter
     new_img = np.zeros([rows, cols])
 
+    # Applies filter to the image by using the weighted average mask for every pixel
     for i in range(1, rows - 1):
         for j in range(1, cols - 1):
             new_img[i, j] = img[i - 1, j - 1] * mask[0, 0] + img[i - 1, j] * mask[0, 1] + img[i - 1, j + 1] * mask[
@@ -154,12 +163,15 @@ def generate_averaging_filter(img):
     new_img = new_img.astype(np.uint8)
     return get_imagetk(new_img)
 
-
+# Generates image w/ median filter
+# Gets the image as a parameter
 def median_filtering(img):
     rows, cols = img.shape  # rows & columns
 
+    # Creates new array for new image w/ filter
     new_img = np.zeros([rows, cols])
 
+    # Applies filter to the image by computing the median and substituting the median for the center pixel
     for i in range(1, rows - 1):
         for j in range(1, cols - 1):
             tmp = [img[i - 1, j - 1],
@@ -172,34 +184,41 @@ def median_filtering(img):
                    img[i + 1, j],
                    img[i + 1, j + 1]]
 
-            tmp = sorted(tmp)
-            new_img[i, j] = tmp[4]
+            tmp = sorted(tmp) # sorts the values
+            new_img[i, j] = tmp[4] # replaces pixel with the median of the tmp array
 
     new_img = new_img.astype(np.uint8)
     return get_imagetk(new_img)
 
-
-# Spatial domain laplacian
+# Generates image w/ spatial domain laplacian highpass filter
+# Gets the image as a parameter
 def highpass_laplacian(img):
+    # Kernel for the laplacian filter
     filter_data = np.array([[0, 1, 0],
                             [1, -4, 1],
                             [0, 1, 0]])
 
+    # Applies the kernel to the image, resulting in a laplacian image
     filtered_img = cv2.filter2D(src=img, ddepth=-1, kernel=filter_data)
 
+    # Formula used in the book
+    # Original image is added to the product of the constant and the filtered image
+    # The constant used is -1 because the center of the kernel is negative
     filter_data2 = img + (-1 * filtered_img)
 
     clip = np.array(np.clip(filter_data2, 0, 255), dtype='uint8')
 
     return get_imagetk(clip)
 
-
+# Generates image w/ unsharp masking filter
+# Gets the image as a parameter
 def unsharp_masking(img):
-    img = (img / 255)
-    blurred_img = cv2.GaussianBlur(img, (31, 31), cv2.BORDER_DEFAULT)
+    img = (img / 255) # normalization
+    blurred_img = cv2.GaussianBlur(img, (31, 31), cv2.BORDER_DEFAULT) # blurring the image
 
-    mask = img - blurred_img
-    final = img + mask
+    # Subtracting blurred image from the original
+    mask = img - blurred_img  
+    final = img + mask # adds mask/filter for unsharp masking to the original image
 
     final = np.clip(final, 0, 255)
     final = cv2.normalize(final, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
@@ -207,43 +226,50 @@ def unsharp_masking(img):
 
     return get_imagetk(final)
 
-
+# Generates image w/ highboost filter
+# Gets the image as a parameter
 def highboost(img):
     img = img / 255
 
-    blurred_img = cv2.GaussianBlur(img, (31, 31), cv2.BORDER_DEFAULT)
+    blurred_img = cv2.GaussianBlur(img, (31, 31), cv2.BORDER_DEFAULT) # blurring the image
 
+    # Subtracting blurred image from the original
     mask = img - blurred_img
-    amplify_param = 5
-    final = img + amplify_param * mask
+    amplify_param = 5 # amplification parameter
+    # adds the product of the mask/filter and amplification parameter to the original image for highboost filtering
+    final = img + amplify_param * mask 
     final = np.clip(final, 0, 255)
     final = cv2.normalize(final, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     final = final.astype('uint8')
 
     return get_imagetk(final)
 
-
+# Generates image w/ sobel gradient
+# Gets the image as a parameter
 def gradient_sobel(img):
-    row, col = np.shape(img)
-    final_img = np.zeros(shape=(row, col))
-    x_grad = np.zeros(shape=(row, col))
-    y_grad = np.zeros(shape=(row, col))
+    row, col = np.shape(img) # Gets nos. of rows and columns
+    final_img = np.zeros(shape=(row, col)) # Initializes new array for the final image
+    x_grad = np.zeros(shape=(row, col)) # Initializes new array for sobel x-gradient
+    y_grad = np.zeros(shape=(row, col)) # Initializes new array for sobel y-gradient
 
+    # Sobel y-gradient mask
     y_sobel = np.array(([[-1, -2, -1],
                          [0, 0, 0],
                          [1, 2, 1]]))
 
+    # Sobel x-gradient mask
     x_sobel = np.array(([[-1, 0, 1],
                          [-2, 0, 2],
                          [-1, 0, 1]]))
 
+    # Apply the mask to the image using the formula
     for i in range(row - 2):
         for j in range(col - 2):
             x_partial = np.sum(np.multiply(x_sobel, img[i:i + 3, j:j + 3]))  # compute partial derivative of x
             y_partial = np.sum(np.multiply(y_sobel, img[i:i + 3, j:j + 3]))  # compute partial derivative of y
-            x_grad[i + 1, j + 1] = x_partial
-            y_grad[i + 1, j + 1] = y_partial
-            final_img[i + 1, j + 1] = np.sqrt(x_partial ** 2 + y_partial ** 2)
+            x_grad[i + 1, j + 1] = x_partial # sobel x-gradient
+            y_grad[i + 1, j + 1] = y_partial # sobel y-gradient
+            final_img[i + 1, j + 1] = np.sqrt(x_partial ** 2 + y_partial ** 2) # combines the x and y gradient to the image
 
     return get_imagetk(final_img), get_imagetk(x_grad), get_imagetk(y_grad)
 
